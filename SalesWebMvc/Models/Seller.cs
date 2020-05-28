7328168a4bc67a9212d476722a4649e1e764d815
Alexandre <alexandre.1054@yahoo.com>
2020-05-28 20:36:58 -0300

    Create other entities and Migration

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double BaseSalary { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Departament Departament { get; set; }
+        public ICollection<SallesRecord> Sales { get; set; } = new List<SallesRecord>();
+    
+
+        public Seller()
+        {
+
+        }
+
+        public Seller(int id, string name, string email, double baseSalary, DateTime birthDate, Departament departament)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BaseSalary = baseSalary;
+            BirthDate = birthDate;
+            Departament = departament;
+        }
+
+
+        public void AddSales(SallesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SallesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
